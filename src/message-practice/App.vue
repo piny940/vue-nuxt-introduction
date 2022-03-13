200fb2782fa28b094c960825bf4d8d08dada5c0d
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-13 16:00:46 +0900

    Archive message-practice app

---
+++ src/message-practice/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" v-on:showClickNum="showClickNum"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    showClickNum(num) {
+      window.alert(num);
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
