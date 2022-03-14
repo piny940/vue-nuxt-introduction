d796983c555ff7f75653f222bf3ad7756bf7b8b8
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-14 15:08:23 +0900

    Try names slot

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="click-box" v-on:click.exact="onClick" v-on:click.shift="onShiftClick">クリックしてください！</div>
   <p>内容: {{ description }}</p>
+  <HelloWorld>
+    <template v-slot:hoge>
+      hogehoge
+    </template>
+    hagehage
+  </HelloWorld>
 </template>
 
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
   name: "app",
+  components: {
+    HelloWorld,
+  },
   data: function() {
     return {
       description: "",
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div><slot name="hoge" /></div>
+    <div><slot /></div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
