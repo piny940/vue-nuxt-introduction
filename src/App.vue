ceb9ce5b4f69c30d59da7b4cc418d280ab093a54
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-14 10:06:05 +0900

    Practice click ditection

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  hoge
+  <div id="click-box" v-on:click.exact="onClick" v-on:click.shift="onShiftClick">クリックしてください！</div>
+  <p>内容: {{ description }}</p>
 </template>
 
 
 <script>
 export default {
   name: "app",
-}
+  data: function() {
+    return {
+      description: "",
+    };
+  },
+  methods: {
+    onShiftClick: function() {
+      this.description = "shift + click!";
+    },
+    onClick: function(e) {
+      if (!e.shift) {
+        this.description = "";
+      }
+    },
+  },
+};
 </script>
+
+<style scoped>
+#click-box {
+  width: auto;
+  min-height: 20px;
+  border: 2px solid blue;
+}
+</style>
